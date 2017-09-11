de5abc1a1df4fa86423419cb67a9de3c9bf74692
kderoulet <kderoulet@yahoo.com>
2017-09-11 16:26:19 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,14 +33,26 @@ if (cardsInPlay.length === 2) {
 }
 }
 
-var flipCard = function (cardId) {
- checkForMatch();
+var flipCard = function () {
+ var cardId = this.getAttribute("data-id");
+ this.setAttribute("src", cards[cardId].cardImage);
  console.log("User flipped " + cards[cardId].rank);
  cardsInPlay.push(cards[cardId].rank);
  console.log(cards[cardId].cardImage);
- console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
+ checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+
+}
+}
+
+createBoard();
 
